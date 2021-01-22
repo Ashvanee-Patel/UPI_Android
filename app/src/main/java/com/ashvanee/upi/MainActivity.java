284c54a7313498cbc28cd3054134109718dbbf56
Ashvanee-Patel <ashvanee1655264@gmail.com>
2021-01-22 16:06:49 +0530

    Initial Project Setup

---
+++ app/src/main/java/com/ashvanee/upi/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ashvanee.upi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
